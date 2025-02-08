2a442e897b36a56deff18f429ba18dab307e1fd9
tseitz <tdseitz10@gmail.com>
2025-02-08 10:48:23 -0600

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+
+  integrations: [mdx(), sitemap(), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
