039a94c59a58eab7bcf1dcaf92231909eb223233
tseitz <tdseitz10@gmail.com>
2025-02-09 10:35:39 -0600

    burn it down and start over

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "T & J Notes";
-export const SITE_DESCRIPTION = "A collection of notes from T & J";
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	<Sidebar.Provider>
+		<AppSidebar />
+		<main>
+			<Sidebar.Trigger />
+			{@render children?.()}
+		</main>
+	</Sidebar.Provider>
+</ParaglideJS>
